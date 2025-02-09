6386a5acecbf6aa1968ea3ad5e7ac97633d7d789
PrathamJain2601 <jainpratham2601@gmail.com>
2025-02-09 10:56:03 +0530

    Ignore dist folder

--- dist/controllers/user/index.controller.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const delete_profile_controller_1 = require("./delete-profile.controller");
-const get_profile_controller_1 = require("./get-profile.controller");
-const update_profile_controller_1 = require("./update-profile.controller");
-const user = { deleteProfile: delete_profile_controller_1.deleteProfile, getProfile: get_profile_controller_1.getProfile, updateProfile: update_profile_controller_1.updateProfile };
-exports.default = user;
